4dc5a727c407ad19d18b770dbc37ca08d862ddc7
stepazel <stepan.zelenka@seznam.cz>
2021-08-08 12:22:34 +0200

    Detail and index templates added

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, Http404
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
 
 
 def index(request) -> HttpResponse:
-    return HttpResponse('Nazdar')
+    latestQuestionList = Question.objects.order_by('-publishedDate')[:5]
+    context = {
+        'latestQuestionList': latestQuestionList
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request: HttpRequest, questionId: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=questionId)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request: HttpRequest, questionId: int) -> HttpResponse:
+    return HttpResponse("You're looking at the results of question %s." % questionId)
+
+
+def vote(request: HttpRequest, questionId: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % questionId)
