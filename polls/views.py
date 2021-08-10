58c2ff2135f7fce5bcdd6705f89a90007f08182c
stepazel <stepan.zelenka@seznam.cz>
2021-08-10 21:53:59 +0200

    Added tests for view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Question, Choice
@@ -11,12 +12,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-publishedDate')[:5]
+        return Question.objects.filter(publishedDate__lte=timezone.now()).order_by('-publishedDate')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(publishedDate__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
